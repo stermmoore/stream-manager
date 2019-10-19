aba3722e3cf81db028f6bbfcaf6198427bda260b
stermmoore <stermmoore@gmail.com>
2019-10-19 13:11:52 +0100

    removed  or renamed cli generated code

---
+++ src/StreamManager/Controllers/UserStreamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StreamManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserStreamController : ControllerBase
+    {
+        private readonly ILogger<UserStreamController> _logger;
+
+        public UserStreamController(ILogger<UserStreamController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
